0f86eeae4ab313acca78af569d059f95c2e9bffd
cristina <2023_cristina.val@iticbcn.cat>
2023-11-29 13:38:39 +0100

    29/11/2023

--- ddl/exercicis.sql
+++ ddl/exercicis.sql
@@ -1,8 +1,8 @@
 /*Exercici 1: Crea una taula anomenada pelicula sense cap restricció, que tingui els camps següents:
-				ID 		□ 	Cadena de caràcters (6)
-				Titulo 	□ 	Cadena de caràcters (70)
-				Anyo 	□ 	numèric
-				Genero 	□ 	Cadena de caràcters (15)																		*/
+    ID 		□ 	Cadena de caràcters (6)
+    Titulo 	□ 	Cadena de caràcters (70)
+    Anyo 	□ 	numèric
+    Genero 	□ 	Cadena de caràcters (15)																		*/
 
 				create table pelicula(
 
@@ -14,8 +14,8 @@
 
 
 /*Exercici 2: Crea una taula anomenada actor sense cap restricció, que tingui els camps següents:
-				ID 		□ 	numèric de 4 xifres
-				Nombre 	□ 	Cadena de caràcters (25)																		*/
+    ID 		□ 	numèric de 4 xifres
+    Nombre 	□ 	Cadena de caràcters (25)																		*/
 
 				create table actor(
 
@@ -26,9 +26,9 @@
 
 
 /*Exercici 3: Crea una taula anomenada participar sense cap restricció que tingui els camps següents:
-				Pelicula_id  □ 	Numèric de 4 xifres
-				Actor_id 	 □ 	Numèric de 4 xifres
-				Orden 		 □ 	Numèric																						*/
+    Pelicula_id  □ 	Numèric de 4 xifres
+    Actor_id 	 □ 	Numèric de 4 xifres
+    Orden 		 □ 	Numèric																						*/
 
 				create table participar (
 
