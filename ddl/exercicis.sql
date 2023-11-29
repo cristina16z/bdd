9bcc1223420c2844fdd56fee91d697a6268b90db
cristina <2023_cristina.val@iticbcn.cat>
2023-11-29 13:42:19 +0100

    29/11/2023

--- ddl/exercicis.sql
+++ ddl/exercicis.sql
@@ -4,26 +4,26 @@
     Anyo 	□ 	numèric
     Genero 	□ 	Cadena de caràcters (15)																		*/
 
-    create table pelicula(
+create table pelicula(
 
-        id char (6),				/*cómo no sabemos si su longitud es variable también vale varchar 			*/
-        Titulo varchar(70),					
-        Anyo year (4),					
-        Genero varchar(15)	
+    id char (6),				/*cómo no sabemos si su longitud es variable también vale varchar 			*/
+    Titulo varchar(70),					
+    Anyo year (4),					
+    Genero varchar(15)	
 
-	);
+);
 
 
 /*Exercici 2: Crea una taula anomenada actor sense cap restricció, que tingui els camps següents:
     ID 		□ 	numèric de 4 xifres
     Nombre 	□ 	Cadena de caràcters (25)																		*/
 
-	create table actor(
+create table actor(
 
-		ID smallint,
-		Nombre varchar(25)
+	ID smallint,
+	Nombre varchar(25)
 				
-    );
+);
 
 
 /*Exercici 3: Crea una taula anomenada participar sense cap restricció que tingui els camps següents:
@@ -31,10 +31,10 @@
     Actor_id 	 □ 	Numèric de 4 xifres
     Orden 		 □ 	Numèric																						*/
 
-	create table participar (
+create table participar (
 
-		pelicula_id smallint,         /*no hace falta poner el (4) porque ya se aclara con el smallint*/
-		actor_id smallint,              /*y saltaría un warning cómo que no hace falta*/
-		orden smallint
+	pelicula_id smallint,         /*no hace falta poner el (4) porque ya se aclara con el smallint*/
+	actor_id smallint,              /*y saltaría un warning cómo que no hace falta*/
+	orden smallint
 				
-    );
+);
